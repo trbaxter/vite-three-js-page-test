71340fa41fb738cd2ac5e87e61f4bc4539136ee1
trbaxter <trb7074@gmail.com>
2025-01-09 12:43:23 -0600

    removed shader comments for now

--- src/components/homepage-animation/particles/shaders/FragmentShader.ts
+++ src/components/homepage-animation/particles/shaders/FragmentShader.ts
@@ -6,16 +6,10 @@ export const fragmentShader =
     varying float vOpacity;
   
     void main() {
-      // Calculate the distance from the center of the particle
       float dist = length(gl_PointCoord - vec2(0.5));
-  
-      // Create a radial falloff for the particle alpha
       float radialAlpha = 1.0 - smoothstep(0.0, 1.0, dist);
-      
-      // Combine radialAlpha with the particle's opacity
       float alpha = radialAlpha * vOpacity * 1.25;
       
-      // Set the fragment color with blending
       gl_FragColor = vec4(vColor, alpha);
     }
   `
