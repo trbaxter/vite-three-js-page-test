d2d5c524c75276fd28e4d8c853a6c0916710c234
trbaxter <trb7074@gmail.com>
2025-01-09 12:41:41 -0600

    organized CombinedShaders

--- src/components/homepage-animation/particles/shaders/CombinedShaders.ts
+++ src/components/homepage-animation/particles/shaders/CombinedShaders.ts
@@ -1,16 +1,20 @@
-import * as THREE from 'three';
 import { vertexShader } from './VertexShader.ts';
 import { fragmentShader } from './FragmentShader.ts';
+import { AdditiveBlending, ShaderMaterial } from 'three';
 
 export function CombinedShaders() {
-  return new THREE.ShaderMaterial({
-    vertexShader,
-    fragmentShader,
-    vertexColors: true,
-    transparent: true,
-    depthTest: false, // Prevents z-fighting between particles
-    depthWrite: false, // Prevents particles from occluding each other
-    blending: THREE.AdditiveBlending,
-    toneMapped: true
-  });
+
+  return new ShaderMaterial(
+    {
+      vertexShader,
+      fragmentShader,
+      vertexColors: true,
+      transparent: true,
+      depthTest: false, // Prevents z-fighting between particles
+      depthWrite: false, // Prevents particles from occluding each other
+      blending: AdditiveBlending,
+      toneMapped: true
+    }
+  );
+
 }
