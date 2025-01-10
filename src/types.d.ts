d44e24ce25a7fbb16442daecee01195a3ec383d8
trbaxter <trb7074@gmail.com>
2025-01-10 08:11:18 -0600

    code clean + optimize imports

--- src/types.d.ts
+++ src/types.d.ts
@@ -4,11 +4,11 @@ import { BrainMaterial } from './components/brain-animation/BrainVeins.tsx';
 import { BrainParticleMaterial } from './components/brain-animation/BrainParticles.tsx';
 
 declare module '@react-three/fiber' {
-    interface ThreeElements {
-        brainMaterial: MaterialNode<THREE.ShaderMaterial, typeof BrainMaterial>;
-        brainParticleMaterial: MaterialNode<
-            THREE.ShaderMaterial,
-            typeof BrainParticleMaterial
-        >;
-    }
+  interface ThreeElements {
+    brainMaterial: MaterialNode<THREE.ShaderMaterial, typeof BrainMaterial>;
+    brainParticleMaterial: MaterialNode<
+      THREE.ShaderMaterial,
+      typeof BrainParticleMaterial
+    >;
+  }
 }
