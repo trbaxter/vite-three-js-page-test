9db71793a4a5a3528ac8b19ea36468be336950b3
trbaxter <trb7074@gmail.com>
2025-01-10 07:45:10 -0600

    removed logic causing visual stutter

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,9 @@
 import Navigation from './components/navigation/HandleNavigation.tsx';
-import { Routes, Route, useLocation } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import { BrainAnimation } from './components/brain-animation/BrainAnimation.tsx';
 import { HomepageAnimation } from './components/homepage-animation/HomepageAnimation.tsx';
-import { useEffect } from 'react';
 
 export default function App() {
-  const location = useLocation();
-
-  useEffect(() => {
-    const isInitialLoad = !document.body.classList.contains('render');
-
-    if (isInitialLoad) {
-
-      document.body.classList.add('initializing');
-      setTimeout(() => {
-        document.body.classList.remove('fade-out', 'initializing');
-        document.body.classList.add('render');
-      }, 200);
-    } else {
-
-      document.body.classList.remove('render');
-      setTimeout(() => {
-        document.body.classList.add('render');
-      }, 60);
-    }
-  }, [location]);
 
   return (
     <>
