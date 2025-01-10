d1e2b28c5da8dc20bc88aea896a97f6bd454ca43
trbaxter <trb7074@gmail.com>
2025-01-10 07:32:08 -0600

    removed comment for now

--- src/Main.tsx
+++ src/Main.tsx
@@ -1,10 +1,9 @@
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
+import App from './App';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import { StrictMode } from 'react';
 
-// Add the "render" class to the body when the app is mounted
 document.body.classList.add('render');
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
