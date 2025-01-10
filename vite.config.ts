7d63b6d3be54971243c87a75aa3e186e481f849a
trbaxter <trb7074@gmail.com>
2025-01-10 08:10:33 -0600

    code clean + optimize imports

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,12 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
-    plugins: [react()],
-    server: {
-        hmr: { overlay: true },
-        watch: {
-            usePolling: true,
-            interval: 100
-        },
-    },
+  plugins: [react()],
+  server: {
+    hmr: { overlay: true },
+    watch: {
+      usePolling: true,
+      interval: 100
+    }
+  }
 });
