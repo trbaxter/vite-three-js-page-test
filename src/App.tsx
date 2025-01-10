9a2515d86ba2587c2ebccf0766e14824f0513bad
trbaxter <trb7074@gmail.com>
2025-01-10 08:11:41 -0600

    code clean + optimize imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import Navigation from './components/navigation/HandleNavigation.tsx';
-import { Routes, Route } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import { BrainAnimation } from './components/brain-animation/BrainAnimation.tsx';
 import { HomepageAnimation } from './components/homepage-animation/HomepageAnimation.tsx';
 
