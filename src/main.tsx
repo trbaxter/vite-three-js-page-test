68453e80b896f767746987a183792bb96b9f53b3
trbaxter <trb7074@gmail.com>
2025-01-09 20:40:20 -0600

    removed unnecessary StrictMode

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
@@ -8,9 +7,9 @@ import { BrowserRouter as Router } from 'react-router-dom';
 document.body.classList.add('render');
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-    <React.StrictMode>
+
       <Router>
         <App />
       </Router>
-    </React.StrictMode>,
+
 );
