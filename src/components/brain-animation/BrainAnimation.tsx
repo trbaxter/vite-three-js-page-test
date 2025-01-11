ddb222cab6e73ac88a89cbe65fb2e679b69b0682
trbaxter <trb7074@gmail.com>
2025-01-10 23:25:40 -0600

    cleanup + refactor

--- src/components/brain-animation/BrainAnimation.tsx
+++ src/components/brain-animation/BrainAnimation.tsx
@@ -1,23 +1,21 @@
 import { Canvas } from '@react-three/fiber';
-import { OrbitControls } from '@react-three/drei';
 import { BrainParticles } from './BrainParticles';
 import { Veins } from './BrainVeins';
-import { createBrainVeinsFromPaths } from './utils';
-import { RotatingBrain } from './RotatingBrain';
+import { brainCurveVectorArray } from '../../utils/brain-animation/brainCurveVectorArray.ts';
+import { RotateGroupFrame } from './RotateGroupFrame.tsx';
 
 export function BrainAnimation() {
-  const curves = createBrainVeinsFromPaths();
+  const curves = brainCurveVectorArray();
 
   return (
     <Canvas camera={{ position: [0, 0, 0.3], near: 0.001, far: 5 }}>
       <color attach="background" args={['black']} />
       <ambientLight />
       <pointLight position={[10, 10, 10]} />
-      <RotatingBrain>
+      <RotateGroupFrame>
         <Veins curves={curves} />
         <BrainParticles curves={curves} />
-      </RotatingBrain>
-      <OrbitControls />
+      </RotateGroupFrame>
     </Canvas>
   );
 }
