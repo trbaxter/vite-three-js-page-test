e72834f91ef08e36ba2dd4c88f3167fd233bef65
trbaxter <trb7074@gmail.com>
2025-01-11 18:15:35 -0600

    actually need this, get error in brain

---
+++ src/types.d.ts
@@ -0,0 +1,13 @@
+import { MaterialNode } from '@react-three/fiber';
+import { BrainMaterial } from './components/brain-animation/BrainVeins.tsx';
+import { BrainParticleMaterial } from './components/brain-animation/BrainParticles.tsx';
+
+declare module '@react-three/fiber' {
+  interface ThreeElements {
+    brainMaterial: MaterialNode<ShaderMaterial, typeof BrainMaterial>;
+    brainParticleMaterial: MaterialNode<
+      ShaderMaterial,
+      typeof BrainParticleMaterial
+    >;
+  }
+}
